74abaac36103177cdd02ce0c3011851a83f7ab87
qmchugh <qmchugh@frederickscompany.com>
2022-02-04 16:18:26 -0500

    Added project files

---
+++ src/__main__.py
@@ -0,0 +1,20 @@
+# RESOURCES:
+# Final Version Perfected (FVP): https://www.lesswrong.com/posts/xfcKYznQ6B9yuxB28
+# * Original article: http://archive.constantcontact.com/fs004/1100358239599/archive/1109511856508.html#LETTER.BLOCK4
+# Binary priorization: https://en.wikipedia.org/wiki/Binary_prioritization
+# A human-driven sort algorithm (MonkeySort): https://leonid.shevtsov.me/post/a-human-driven-sort-algorithm-monkeysort/
+
+
+from pathlib import Path
+
+
+def get_task_list(file_path: Path):
+    """Gets the task list from a plain text file and returns a list of tasks."""
+
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
